bd14bc147499ed1aeae2f2724d682833923d460b
MuhammadFebriArdiyanSaputra <m.febri123.cc@gmail.com>
2025-07-09 08:08:30 +0700

    first commit

---
+++ resources/js/components/Navigasi.jsx
@@ -0,0 +1,7 @@
+export default function Navigasi() {
+    return (
+        <div className="p-4 bg-gray-100 rounded-xl">
+            <h1 className="text-xl font-bold">Navigasi Buku Tenaga Kerja</h1>
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.jsx",
+];
